843ec9677fb820ac408544cd672b9a892c8c5956
Boulay Arnaud <boulay_b@localhost.localdomain>
2015-03-22 16:06:36 +0100

    Get_Next_Line

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/*
+** get_next_line.h for get_next_line in /home/boulay_b/Rendu/CPE_2014_get_next_line
+**
+** Made by arnaud boulay
+** Login   <boulay_b@epitech.net>
+**
+** Started on  Mon Nov 17 18:53:27 2014 arnaud boulay
+** Last update Fri Jan 30 16:03:20 2015 arnaud boulay
+*/
+
+#ifndef	GET_NEXT_LINE_H_
+# define GET_NEXT_LINE_H_
+
+# define BUFFER_SIZE 4096
+
+char	*get_next_line(const int fd);
+
+#endif /* !GET_NEXT_LINE_H_ */
